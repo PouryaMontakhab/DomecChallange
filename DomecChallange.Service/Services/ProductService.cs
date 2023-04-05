53ab66e2d40a155366c12c81ad5bd49d87b8ed92
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2023-04-05 21:04:22 +0200

    feat: First implemented IProductService

--- DomecChallange.Service/Services/ProductService.cs
+++ DomecChallange.Service/Services/ProductService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DomecChallange.Domain.Entities;
+using DomecChallange.Dtos.Codes;
+using DomecChallange.Service.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,42 @@ using System.Threading.Tasks;
 
 namespace DomecChallange.Service.Services
 {
-    public class ProductService
+    public class ProductService : IProductService
     {
+        #region Props
+
+        #endregion
+        #region Ctor
+        public ProductService()
+        {
+
+        }
+        #endregion
+        #region Methods
+        public async Task<StatusDto> CreateAsync(Product item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Product> GetAsync(Guid uniqueId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Product> GetAsync(int code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<StatusDto> UpdateAsync(Product item)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
     }
 }
