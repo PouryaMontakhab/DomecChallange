75a78c91db163ca3fafb43a4de4680ba8427435e
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2023-04-05 19:16:32 +0200

    feat: Added dbcontext

---
+++ DomecChallange.Data/Context/DomecChallangeDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DomecChallange.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomecChallange.Data.Context
+{
+    public class DomecChallangeDbContext: DbContext
+    {
+        #region Dbsets
+        public DbSet<Product> Products { get; set; }
+        #endregion
+        public DomecChallangeDbContext(DbContextOptions<DomecChallangeDbContext> options) : base(options)
+        {
+        }
+    }
+}
