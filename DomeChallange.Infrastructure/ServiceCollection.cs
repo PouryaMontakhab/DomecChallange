834616f872d0ed0cfb1c3f8cb2a534209fcbf758
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2023-04-05 20:35:29 +0200

    feat: Added service collection class

---
+++ DomeChallange.Infrastructure/ServiceCollection.cs
@@ -0,0 +1,22 @@
+﻿using DomecChallange.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomeChallange.Infrastructure
+{
+    public static class ServiceCollection
+    {
+        public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DomecChallangeDbContext>(options =>
+                options.UseInMemoryDatabase(databaseName: "DomecChallangeSystemContext"));
+            services.AddScoped<DomecChallangeDbContext>();
+        }
+    }
+}
