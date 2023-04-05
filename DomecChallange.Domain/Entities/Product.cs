8cdc281fa7b8813104715192089c0fade190edfb
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2023-04-05 18:57:20 +0200

    feat: Added some properties to product model

--- DomecChallange.Domain/Entities/Product.cs
+++ DomecChallange.Domain/Entities/Product.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,15 @@ namespace DomecChallange.Domain.Entities
 {
     public class Product
     {
-
+        public Product()
+        {
+            this.UniqueId = Guid.NewGuid();
+        }
+        [Key]
+        public Guid UniqueId { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
     }
 }
