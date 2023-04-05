a7fff5109d2efcd3780e1466d441953aaf34bec6
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2023-04-05 18:49:56 +0200

    feat: Added product domain model

---
+++ DomecChallange.Domain/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomecChallange.Domain.Entities
+{
+    public class Product
+    {
+
+    }
+}
