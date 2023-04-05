c5e167758d0b7dbd757e2ca4b7c2fc31f313fc92
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2023-04-05 23:39:56 +0200

    feat: Added CodeGenerator class in order to generate automaticaly codes

--- DomecChallange.Data/Context/DomecChallangeDbContext.cs
+++ DomecChallange.Data/Context/DomecChallangeDbContext.cs
@@ -1,5 +1,7 @@
-﻿using DomecChallange.Domain.Entities;
+﻿using DomecChallange.Data.Codes;
+using DomecChallange.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory.ValueGeneration.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +10,20 @@ using System.Threading.Tasks;
 
 namespace DomecChallange.Data.Context
 {
-    public class DomecChallangeDbContext: DbContext
+    public class DomecChallangeDbContext : DbContext
     {
         #region Dbsets
         public DbSet<Product> Products => Set<Product>();
         #endregion
         public DomecChallangeDbContext(DbContextOptions<DomecChallangeDbContext> options) : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(code =>
+            {
+                code.Property(p => p.Code).HasValueGenerator<CodeGenerator>();
+            });
         }
     }
 }
