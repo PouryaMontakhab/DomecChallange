55bb13ac2f58218cb401be11bb0efc82d3dd06fd
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2023-04-05 20:55:29 +0200

    feat: Added StatusDto class

---
+++ DomecChallange.Dtos/Codes/StatusDto.cs
@@ -0,0 +1,17 @@
+﻿using DomecChallange.Dtos.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomecChallange.Dtos.Codes
+{
+    public class StatusDto
+    {
+        public string Message { get; set; }
+        public dynamic ReturnId { get; set; }
+        public dynamic ReturnModel { get; set; }
+        public StatusEnum Status { get; set; }
+    }
+}
