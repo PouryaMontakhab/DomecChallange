2b19326276c63ef5a92eefbfbdf60b5e2d8c9693
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2023-04-06 15:58:03 +0200

    feat: Added user dtos

---
+++ DomecChallange.Dtos/UserDtos/UserAuthDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DomecChallange.Dtos.UserDtos
+{
+    public class UserAuthDto
+    {
+        public string UserName { get; set; }
+        public string BearerToken { get; set; }
+        public List<UserCliam> Claims { get; set; } = new();
+    }
+}
---
+++ DomecChallange.Dtos/UserDtos/UserCliam.cs
@@ -0,0 +1,8 @@
+﻿namespace DomecChallange.Dtos.UserDtos
+{
+    public class UserCliam
+    {
+        public string ClaimType { get; set; }
+        public string ClaimValue { get; set; }
+    }
+}
