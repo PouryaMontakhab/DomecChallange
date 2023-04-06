785bd2e1950dbe4f7442f9bb21f6e85481d8dfea
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2023-04-06 15:53:50 +0200

    feat: Added auth controller

---
+++ DomecChallange/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using DomecChallange.Service.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DomecChallange.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class AuthController : ControllerBase
+    {
+        #region Props
+        private readonly IUserService _userService;
+        #endregion
+        #region Ctor
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        #endregion
+        #region Methods
+        public async Task<IActionResult> SignIn()
+        {
+
+        }
+        #endregion
+    }
+}
